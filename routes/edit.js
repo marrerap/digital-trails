35935dc32f33599e1ead7ea7ba8c6dab5fe54f4d
Roderic Russell <rrr5458@gmail.com>
2021-08-03 10:53:27 -0400

    got Post in trails to work

--- routes/edit.js
+++ routes/edit.js
@@ -1,13 +1,28 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
 const db = require('../models')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+    let hiker = db.Hiker
     res.render('edit', {
-        title: 'Edit'
+        title: 'Edit',
+        hiker: hiker
     })
 });
+
+router.post("/", function (req, res, next) {
+    const input = document.getElementById('bioForm')
+    db.Hiker.update(
+      {bio: input.value},
+    )
+    .then(function(rowsUpdated) {
+        console.log(input.value)
+      res.redirect('/profile')
+    })
+    .catch(next)
+   })
 
 
 module.exports = router;
